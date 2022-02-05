d0c5b3c96ae268fe4f5c2d66d60ba6f3e295bb14
Mohammad Dehqani <dehqani-m@amelsystem.com>
2022-02-05 10:11:46 +0330

    initial commit

---
+++ convert-number-mony-to-persina-string-mony/NumberExtensionMethods.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeNameSpace
+{
+    public static class NumberExtensionMethods
+    {
+
+
+        public static string ToStringMony(this decimal number)
+        {
+            var str = "";
+            number /= 10;
+            ConvertToThousand(number, ref str);
+
+            return str;
+        }
+
+        private static void ConvertToThousand(decimal number, ref string str)
+        {
+
+            if (number >= 1000000000)
+            {
+                if (number % 1000000000 > 0)
+                {
+                    str += $" {(int)(number / 1000000000)} میلیارد و";
+                    ConvertToThousand(number % 1000000000, ref str);
+                }
+                else
+                {
+                    str += $" {(int)(number / 1000000000)} میلیارد تومان ";
+                }
+
+            }
+
+            else if (number >= 1000000)
+            {
+                if (number % 1000000 > 0)
+                {
+                    str += $" { (int)(number / 1000000)} میلیون و";
+                    ConvertToThousand(number % 1000000, ref str);
+
+                }
+                else
+                {
+                    str += $" { (int)(number / 1000000)}میلیون تومان ";
+                }
+
+            }
+
+            else if (number >= 1000)
+            {
+                if (number % 1000 > 0)
+                {
+                    str += $"{(int)(number / 1000)}   هزار و ";
+                    ConvertToThousand(number % 1000, ref str);
+
+
+                }
+                else
+                {
+                    str += $"{(int)(number / 1000)} هزار تومان";
+                }
+            }
+            else
+            {
+                str += $"{number:0} تومان";
+            }
+
+        }
+
+    }
+}
